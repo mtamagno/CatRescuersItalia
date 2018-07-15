e91fe311adf9aa7199c8ea2afcfd6c2a428748ac
mtamagno <thegreatherochrono@hotmail.it>
2018-07-15 12:04:43 +0200

    modifiche

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,10 +1,22 @@
 const express = require("express");
 const router = express.Router();
 
+//Load user model
+
+const User = require("../../models/User");
+
 // @route GET api/users/test
 // @desc Tests users route
 // @access pubblic route
 
 router.get("/test", (req, res) => res.json({ msg: "Users Works" }));
+
+// @route GET api/users/register
+// @desc      Register a user
+// @access    pubblic route
+
+router.post("/register", (req, res) => {
+  User.findOne({ email: req.body.email });
+});
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
 
 const users = require("./routes/api/users");
 const posts = require("./routes/api/posts");
@@ -9,6 +10,10 @@ const app = express();
 
 //DB Config
 const db = require("./config/keys").mongoURI;
+
+//BOdyParser
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 //Connect To MongoDB
 mongoose
